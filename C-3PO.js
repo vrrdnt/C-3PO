ed218d41ec84d3ce6a85be0eef2b60cf5431f4a6
Will Swoveland <will.arctic@gmail.com>
2020-07-20 21:51:50 -0700

    Update C-3PO.js

--- C-3PO.js
+++ C-3PO.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const shell = require('shelljs');
 const {Translate} = require('@google-cloud/translate').v2;
 const translate = new Translate();
 
@@ -7,10 +8,24 @@ const client = new Discord.Client();
 const config = require("./config.json");
 
 client.on("ready", () => {
-    console.log("The bot is ready.");
+    console.log("The bot is online.");
+	client.user.setActivity("C-3PO, human/cyborg relations");
 });
 
 client.on("message", (message) => {
+
+	// Separate command and args 
+	const args = message.content.slice(config.prefix.length).trim().split(/ config.prefix/g);
+	const command = args.shift().toLowerCase();
+
+	if (command === "restart") {
+    if (!message.member.roles.cache.has(config.adminid)) {
+        return message.channel.send("Reboot command denied.")
+    }
+    message.channel.send('C-3PO, human/cyborg relations, rebooting.')
+        .then(() => client.destroy())
+        .then(() => shell.exec('./restart.sh'));
+}
 
     var text;
     let target = ['af', 'sq', 'ar', 'hy', 'az', 'eu', 'be', 'bg', 'ca', 'zh-CN', 'zh-TW', 'hr', 'cs', 'da', 'nl', 'en', 'et', 'tl', 'fi', 'fr', 'gl', 'ka', 'de', 'el', 'ht', 'iw', 'hi', 'hu', 'is', 'id', 'ga', 'it', 'ja', 'ko', 'lv', 'lt', 'mk', 'ms', 'mt', 'no', 'fa', 'pl', 'pt', 'ro', 'ru', 'sr', 'sk', 'sl', 'es', 'sw', 'sv', 'th', 'tr', 'uk', 'ur', 'vi', 'cy', 'yi'];
@@ -28,7 +43,7 @@ client.on("message", (message) => {
     if (!message.author.bot) {
         var text = message.content.toString();
         var chance = Math.random();
-		if (chance < 0.1)
+		if (chance < 0.2)
         translateText(message.content, randomTarget);
     }
 
