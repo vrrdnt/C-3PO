0a12571e970c1a4e1e6599bb9bb80b026219d1c5
vrrdnt <will.arctic@gmail.com>
2020-07-21 12:36:17 -0700

    finally done

--- C-3PO.js
+++ C-3PO.js
@@ -1,50 +1,61 @@
 const Discord = require('discord.js');
+const fs = require('fs')
 const shell = require('shelljs');
-const {Translate} = require('@google-cloud/translate').v2;
+const {
+    Translate
+} = require('@google-cloud/translate').v2;
 const translate = new Translate();
 
 
 const client = new Discord.Client();
 const config = require("./config.json");
+const langtoflag = require("./langtoflag.json");
 
 client.on("ready", () => {
     console.log("The bot is online.");
-	client.user.setActivity("C-3PO, human/cyborg relations");
+    client.user.setActivity("human/cyborg relations");
 });
 
 client.on("message", (message) => {
 
-	// Separate command and args 
-	const args = message.content.slice(config.prefix.length).trim().split(/ config.prefix/g);
-	const command = args.shift().toLowerCase();
+    // Separate command and args
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
 
-	if (command === "restart") {
-    if (!message.member.roles.cache.has(config.adminid)) {
-        return message.channel.send("Reboot command denied.")
+    if (command === "restart") {
+        if (!message.member.roles.cache.has(config.adminid)) {
+            return message.channel.send("Reboot command denied.")
+        }
+        message.channel.send('C-3PO, human/cyborg relations, rebooting.')
+            .then(() => client.destroy())
+            .then(() => shell.exec('./restart.sh'));
     }
-    message.channel.send('C-3PO, human/cyborg relations, rebooting.')
-        .then(() => client.destroy())
-        .then(() => shell.exec('./restart.sh'));
-}
 
-    var text;
-    let target = ['af', 'sq', 'ar', 'hy', 'az', 'eu', 'be', 'bg', 'ca', 'zh-CN', 'zh-TW', 'hr', 'cs', 'da', 'nl', 'en', 'et', 'tl', 'fi', 'fr', 'gl', 'ka', 'de', 'el', 'ht', 'iw', 'hi', 'hu', 'is', 'id', 'ga', 'it', 'ja', 'ko', 'lv', 'lt', 'mk', 'ms', 'mt', 'no', 'fa', 'pl', 'pt', 'ro', 'ru', 'sr', 'sk', 'sl', 'es', 'sw', 'sv', 'th', 'tr', 'uk', 'ur', 'vi', 'cy', 'yi'];
-    var randomTarget = target[Math.floor(Math.random() * target.length)];
 
+    let target = ['af', 'sq', 'am', 'ar', 'hy', 'az', 'eu', 'be', 'bn', 'bs', 'bg', 'ca', 'ceb', 'ny', 'zh-cn', 'zh-tw', 'co', 'hr', 'cs', 'da', 'nl',
+        'en', 'eo', 'et', 'tl', 'fi', 'fr', 'fy', 'gl', 'ka', 'de', 'el', 'gu', 'ht', 'ha', 'haw', 'iw', 'hi', 'hmn', 'hu', 'is', 'ig', 'id', 'ga',
+        'it', 'ja', 'jw', 'kn', 'kk', 'km', 'ko', 'ku', 'ky', 'lo', 'la', 'lv', 'lt', 'lb', 'mk', 'mg', 'ms', 'ml', 'mt', 'mi', 'mr', 'mn', 'my',
+        'ne', 'no', 'ps', 'fa', 'pl', 'pt', 'ma', 'ro', 'ru', 'sm', 'gd', 'sr', 'st', 'sn', 'sd', 'si', 'sk', 'sl', 'so', 'es', 'su', 'sw', 'sv',
+        'tg', 'ta', 'te', 'th', 'tr', 'uk', 'ur', 'uz', 'vi', 'cy', 'xh', 'yi', 'yo', 'zu'
+    ];
+    let randomTarget = target[Math.floor(Math.random() * target.length)];
+
+    let text;
     async function translateText() {
         let [translations] = await translate.translate(text, randomTarget);
         translations = Array.isArray(translations) ? translations : [translations];
         translations.forEach((translation, i) => {
-            message.channel.send(`${translation}`);
+            message.channel.send(`${translation}`)
+                .then(m => m.react(langtoflag[randomTarget]))
         });
     }
 
-	
+
     if (!message.author.bot) {
-        var text = message.content.toString();
-        var chance = Math.random();
-		if (chance < 0.2)
-        translateText(message.content, randomTarget);
+        text = message.content.toString();
+        let chance = Math.random();
+        if (chance < 0.1)
+            translateText(message.content);
     }
 
 })
