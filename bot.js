a15afde3f52a9a02ccf421804843078a580ba647
vrrdnt <will.arctic@gmail.com>
2020-07-23 14:25:20 -0700

    pretty

--- bot.js
+++ bot.js
@@ -1,16 +1,18 @@
 const Discord = require('discord.js');
 const fs = require('fs')
 const shell = require('shelljs');
-const {Translate} = require('@google-cloud/translate').v2;
+const {
+    Translate
+} = require('@google-cloud/translate').v2;
 
 const projectId = process.env.GOOGLE_PROJECT_ID;
 
 const translate = new Translate({
-  projectId: projectId,
-  credentials: {
-    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.GOOGLE_CLIENT_EMAIL
-  }
+    projectId: projectId,
+    credentials: {
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        client_email: process.env.GOOGLE_CLIENT_EMAIL
+    }
 });
 
 
@@ -20,13 +22,14 @@ const langtoflag = require("./langtoflag.json");
 
 client.on("ready", () => {
     console.log("The bot is online.");
-    console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`)
+    console.log(
+        `Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`)
     client.user.setActivity("human/cyborg relations");
 });
 
 client.on("message", (message) => {
 
-    if(message.author.bot) return;
+    if (message.author.bot) return;
 
     // Separate command and args
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
@@ -38,8 +41,8 @@ client.on("message", (message) => {
         }
         if (message.content.includes(prefix)) {
             message.channel.send('C-3PO, human/cyborg relations, rebooting.')
-                .then(() => client.destroy())
-                .then(() => shell.exec('./restart.sh'));
+                .then(() => client.destroy()
+                    .then(() => shell.exec('heroku ps:restart worker.1')));
         }
     }
 
@@ -48,7 +51,8 @@ client.on("message", (message) => {
         'en', 'eo', 'et', 'tl', 'fi', 'fr', 'fy', 'gl', 'ka', 'de', 'el', 'gu', 'ht', 'ha', 'haw', 'iw', 'hi', 'hmn', 'hu', 'is', 'ig', 'id', 'ga',
         'it', 'ja', 'jw', 'kn', 'kk', 'km', 'ko', 'ku', 'ky', 'lo', 'la', 'lv', 'lt', 'lb', 'mk', 'mg', 'ms', 'ml', 'mt', 'mi', 'mr', 'mn', 'my',
         'ne', 'no', 'ps', 'fa', 'pl', 'pt', 'ma', 'ro', 'ru', 'sm', 'gd', 'sr', 'st', 'sn', 'sd', 'si', 'sk', 'sl', 'so', 'es', 'su', 'sw', 'sv',
-        'tg', 'ta', 'te', 'th', 'tr', 'uk', 'ur', 'uz', 'vi', 'cy', 'xh', 'yi', 'yo', 'zu'];
+        'tg', 'ta', 'te', 'th', 'tr', 'uk', 'ur', 'uz', 'vi', 'cy', 'xh', 'yi', 'yo', 'zu'
+    ];
     let randomTarget = target[Math.floor(Math.random() * target.length)];
 
     let text;
